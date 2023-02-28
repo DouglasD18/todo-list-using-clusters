58da2859458dabe1dd56be8405933f950807524a
Douglinhas <daguiaralcantara@gmail.com>
2023-02-27 22:02:03 -0300

    git commit -m "feat: create CreateTaskController"

---
+++ src/controllers/create-task.ts
@@ -0,0 +1,36 @@
+import { ValidateBody } from "../interfaces/validate-body";
+import { CreateTask } from '../interfaces/useCases';
+import { HttpRequest, HttpResponse } from '../interfaces/http';
+import { MissingParamError, ServerError } from "../interfaces/errors";
+
+export class CreateTaskController {
+  constructor(
+    private validateBody: ValidateBody,
+    private createTask: CreateTask
+  ) {}
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const { body } = httpRequest;
+
+      const isValid = this.validateBody.isValid(body);
+      if (typeof isValid === "string") {
+        return {
+          statusCode: 400,
+          body: new MissingParamError(isValid)
+        }
+      }
+
+      const task = await this.createTask.create(body);
+      return {
+        statusCode: 201,
+        body: task
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError()
+      }
+    }
+  }
+}
