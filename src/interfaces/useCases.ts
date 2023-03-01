1888304828b9c7a457d0ae684f3e227a5e53fb5c
Douglinhas <daguiaralcantara@gmail.com>
2023-03-01 11:36:37 -0300

    git commit -m "feat: create ReadTasks and UpdateTask interface"

--- src/interfaces/useCases.ts
+++ src/interfaces/useCases.ts
@@ -3,3 +3,11 @@ import { Task } from './task';
 export interface CreateTask {
   create(task: Task): Promise<Task>
 }
+
+export interface ReadTasks {
+  read(): Promise<Task[]>
+}
+
+export interface UpdateTask {
+  update(request: { name: string, task: Task }): Promise<Task>
+}
