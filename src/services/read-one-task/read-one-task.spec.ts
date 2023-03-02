579537687eaa031e1c8217664e4986aead55dcbb
Douglinhas <daguiaralcantara@gmail.com>
2023-03-02 16:07:19 -0300

    git commit -m "test: test ReadOneTask Service"

---
+++ src/services/read-one-task/read-one-task.spec.ts
@@ -0,0 +1,46 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+import { Task } from "../../interfaces/task";
+import { ReadOneTaskService } from "./read-one-task";
+import { create } from "../../utils/tasks-manipulation";
+
+const file = path.resolve("tasks.json");
+
+const makeFakeTask = (): Task => ({
+  name: "Task",
+  description: "A Task",
+  isDone: true
+})
+
+describe("ReadOneTask Service", () => {
+  afterEach(async () => {
+    await fs.writeFile(file, []);
+  })
+
+  it("Should return an empty array if do not have any Task with the same name", async () => {
+    const sut = new ReadOneTaskService();
+
+    await create(makeFakeTask());
+    const task = await sut.read("Code");
+
+    expect(task).toEqual([]);
+  })
+
+  it("Should return the correct Task", async () => {
+    const sut = new ReadOneTaskService();
+
+    await create(makeFakeTask());
+    await create({
+      name: "Code",
+      description: "Code to much",
+      isDone: false
+    })
+    const task = await sut.read("Code");
+
+    expect(task).toEqual({
+      name: "Code",
+      description: "Code to much",
+      isDone: false
+    });
+  })
+})
