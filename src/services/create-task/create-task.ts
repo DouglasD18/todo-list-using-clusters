129ea720c464bb23be9d0d4ce6ee1cf48afc9562
Douglinhas <daguiaralcantara@gmail.com>
2023-03-02 15:01:47 -0300

    git commit -m "feat: create CreateTask Service"

---
+++ src/services/create-task/create-task.ts
@@ -0,0 +1,19 @@
+import { Task } from "../../interfaces/task";
+import { CreateTask } from "../../interfaces/useCases";
+import { create, read } from "../../utils/tasks-manipulation";
+
+export class CreateTaskService implements CreateTask {
+  async create(task: Task): Promise<Task> {
+    const tasks = await read();
+    if (tasks) {
+      const exists = tasks.find(t => t.name === task.name);
+      if (!exists) {
+        await create(task);
+      }
+    } else {
+      await create(task);
+    }
+
+    return task;
+  }
+}
