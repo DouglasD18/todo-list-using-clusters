030bb0445a42e8d0ab075cc1d67d2523f1ec7db2
Douglinhas <daguiaralcantara@gmail.com>
2023-03-04 10:55:42 -0300

    git commit -m "test: create CORS middleware"

--- src/config/middlewares.ts
+++ src/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { Express } from "express";
 import { contentType } from "../middlewares/content-type/content-type";
+import { cors } from "../middlewares/cors/cors";
 
 export default (app: Express): void => {
   app.use(contentType);
+  app.use(cors)
 }
